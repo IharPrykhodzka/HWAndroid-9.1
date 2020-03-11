68f1742adfab79af009a41d7596ed6d63ebf3a34
Igor Prykhodzka <Kvait@bk.ru>
2020-03-11 13:47:36 +0300

    Second commit

--- app/src/main/java/com/example/hwandroid91v2/MainActivity.java
+++ app/src/main/java/com/example/hwandroid91v2/MainActivity.java
@@ -2,19 +2,16 @@ package com.example.hwandroid91v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import java.util.Locale;
 
-class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private String selectedColor;
     private Integer chooseColor;
@@ -28,8 +25,6 @@ class MainActivity extends Activity implements View.OnClickListener {
         Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnOK).setOnClickListener(this);
-
         btnOK2 = findViewById(R.id.btnOK);
         spinnerColors = findViewById(R.id.spinnerThemes);
         ArrayAdapter arrayAdapter2 = ArrayAdapter.createFromResource(this, R.array.spinnerThemes, android.R.layout.simple_spinner_item);
@@ -42,7 +37,6 @@ class MainActivity extends Activity implements View.OnClickListener {
             public void onItemSelected(AdapterView adapterView, View view, int position, long id) {
                 String[] spinnerLanguages = getResources().getStringArray(R.array.spinnerThemes);
                 selectedColor = spinnerLanguages[position];
-
             }
 
             @Override
@@ -69,53 +63,16 @@ class MainActivity extends Activity implements View.OnClickListener {
                 } else if (selectedColor.equals("Blue")) {
                     chooseColor = 2;
                 }
+                if (chooseColor == 0) {
+                    Utils.changeToTheme(MainActivity.this, Utils.THEME_BLACK);
+                } else if (chooseColor == 1) {
+                    Utils.changeToTheme(MainActivity.this, Utils.THEME_GREEN);
+                } else if (chooseColor == 2) {
+                    Utils.changeToTheme(MainActivity.this, Utils.THEME_BLUE);
+                }
+
 
             }
         });
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (chooseColor == 0) {
-            Utils.changeToTheme(this, Utils.THEME_BLACK);
-        }else if (chooseColor == 1) {
-            Utils.changeToTheme(this, Utils.THEME_GREEN);
-        }else if (chooseColor == 2) {
-            Utils.changeToTheme(this, Utils.THEME_BLUE);
-        }
-    }
-
-    public static class Utils {
-
-        private static int sTheme;
-
-        public final static int THEME_BLACK = 0;
-        public final static int THEME_GREEN = 1;
-        public final static int THEME_BLUE = 2;
-
-
-        public static void changeToTheme(Activity activity, int theme) {
-            sTheme = theme;
-            activity.finish();
-
-            activity.startActivity(new Intent(activity, activity.getClass()));
-
-        }
-
-
-        public static void onActivityCreateSetTheme(Activity activity) {
-            switch (sTheme) {
-                default:
-                case THEME_BLACK:
-                    activity.setTheme(R.style.MyBlackTheme);
-                    break;
-                case THEME_GREEN:
-                    activity.setTheme(R.style.MyGreenTheme);
-                    break;
-                case THEME_BLUE:
-                    activity.setTheme(R.style.MyBlueTheme);
-                    break;
-            }
-        }
     }
 }
